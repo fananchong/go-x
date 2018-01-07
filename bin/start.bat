bf3ef01324ad64d278487170953135d3d3b39a4f
fananchong <fananchong@126.com>
2018-01-07 18:01:49 +0800

    add arg: -cfg

--- bin/start.bat
+++ bin/start.bat
@@ -1,7 +1,7 @@
 
-set ETCD_HOSTS=192.168.1.4:12379,192.168.1.4:22379,192.168.1.4:32379
+set ETCD_HOSTS="192.168.1.4:12379, 192.168.1.4:22379, 192.168.1.4:32379"
 
-start base.exe --etcdHosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
-start gateway.exe --etcdHosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
-start room.exe --etcdHosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
-start login.exe --etcdHosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
+start base.exe -cfg config.toml --etcd-hosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
+start gateway.exe -cfg config.toml --etcd-hosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
+start room.exe -cfg config.toml --etcd-hosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
+start login.exe -cfg config.toml --etcd-hosts=%ETCD_HOSTS% --log_dir=./log -stderrthreshold 0
--- common/args.go
+++ common/args.go
@@ -1,11 +1,14 @@
 package common
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/fananchong/multiconfig"
 )
 
 type ArgsBase struct {
-	CfgPath    string   // 配置路径
 	Version    string   // 版本号
 	ExternalIp string   // 外网ip（包括端口。格式 ip:port）
 	IntranetIp string   // 内网ip（包括端口。格式 ip:port）
@@ -13,10 +16,10 @@ type ArgsBase struct {
 }
 
 type ArgsEtcd struct {
-	Hosts          []string `default:[]`
-	NodeType       int      `default:"0"`
-	WatchNodeTypes []int    `default:[]`
-	PutInterval    int      `default:"1"`
+	Hosts          []string `default:""`  // etcd主机IP列表
+	NodeType       int      `default:"0"` // 本节点类型。填0，则本节点不会上报自身信息给etcd。
+	WatchNodeTypes []int    `default:""`  // 本节点要watch其他节点的节点类型
+	PutInterval    int      `default:"1"` // 本节点上报信息间隔，单位秒
 }
 
 type IArgs interface {
@@ -30,8 +33,23 @@ type IArgsBase interface {
 	Init(derived IArgs)
 }
 
+var (
+	cfgpath = flag.String("cfg", "", "path of cfg file")
+)
+
 func (this *ArgsBase) Init(derived IArgs) {
-	m := multiconfig.NewWithPath("config.toml")
+	index := 0
+	for i, v := range os.Args {
+		if v == "-cfg" || v == "--cfg" {
+			index = i + 1
+		}
+	}
+	cfg := ""
+	if index != 0 && index < len(os.Args) {
+		cfg = os.Args[index]
+		fmt.Println("cfg file:", cfg)
+	}
+	m := multiconfig.NewWithPath(cfg)
 	m.MustLoad(derived)
 	derived.OnInit()
 }

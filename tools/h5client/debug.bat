b775fa0314d61bd42139d029f33ab0161b939c12
fananchong <fananchong@126.com>
2018-03-24 12:31:52 +0800

    h5client - add webdevjs

--- tools/h5client/debug.bat
+++
@@ -1,6 +0,0 @@
-set CURPATH=%~dp0\src
-echo %CURPATH%
-
-if not exist %CURPATH%\..\userdata (mkdir %CURPATH%\..\userdata)
-
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files --disable-web-security --user-data-dir=%CURPATH%\..\userdata --app=file:///%CURPATH%/index.html
--- tools/h5client/webpack.config.js
+++ tools/h5client/webpack.config.js
@@ -1,9 +1,21 @@
+var webpack = require('webpack');
+
 module.exports = {
+  mode: 'development',
+  context: __dirname,
   entry: [
-    "./src/index.js",
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+    "./src/index.js"
+  ],
+  devtool: '#source-map',
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   output: {
     path: __dirname,
+    publicPath: '/',
     filename: "./src/bundle.js"
   },
   node: {

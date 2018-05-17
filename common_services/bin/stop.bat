96ab01cc4a4ea9f6c90f91d8d4df9f3dd7d1c110
fananchong <fananchong@126.com>
2018-05-17 22:55:13 +0800

    修改启动脚本

--- common_services/bin/stop.bat
+++ common_services/bin/stop.bat
@@ -1,3 +1,4 @@
 taskkill /F /IM login.exe
 taskkill /F /IM gateway.exe
 taskkill /F /IM hub.exe
+taskkill /F /IM WhatsMyIP.exe
--- example1_iogame/bin/stop.bat
+++ example1_iogame/bin/stop.bat
@@ -3,3 +3,4 @@ taskkill /F /IM gateway.exe
 taskkill /F /IM lobby.exe
 taskkill /F /IM room.exe
 taskkill /F /IM hub.exe
+taskkill /F /IM WhatsMyIP.exe

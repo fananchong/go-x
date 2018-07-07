d59978577864217544e5921f8a129f2dd3b6671d
fananchong <fananchong@126.com>
2018-07-07 17:24:43 +0800

    k8s test

--- build.sh
+++ build.sh
@@ -8,11 +8,17 @@ docker run --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD":/go/src/githu
 
 docker build -t go-x .
 
-docker tag go-x:latest fananchong/go-x:latest
+docker tag go-x:latest 127.0.0.1:5000/fananchong/go-x:latest
 
 set +ex
 
-docker push fananchong/go-x:latest
+docker push 127.0.0.1:5000/fananchong/go-x:latest
 
 
-
+#kubectl apply -f k8s/namespace.yaml
+#kubectl apply -f k8s/service_account.yaml
+#kubectl create -f k8s/redis.yaml
+kubectl delete -f k8s/login.yaml
+kubectl create -f k8s/login.yaml
+kubectl delete -f k8s/gateway.yaml
+kubectl create -f k8s/gateway.yaml
